2f37a252563b91fded2bcb3956117621e90bdd66
toneja <toneyja@oregonstate.edu>
2024-12-09 15:14:05 -0800

    add basic image classification script

---
+++ classify_imgs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/evn python3
+
+import os
+import sys
+from ultralytics import YOLO
+
+
+def classify_images(model_path, image_dir):
+    # Load the previously trained model
+    model = YOLO(model_path)
+    # Loop through the testing directory
+    for image_name in os.listdir(image_dir):
+        image_path = os.path.join(image_dir, image_name)
+        if not image_name.lower().endswith((".jpg", ".jpeg")):
+            continue
+        # Evaluate each image
+        results = model(image_path)
+        print(results)
+        print(f"Processed {image_name}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
+        model_path = f"{sys.argv[1]}/runs/train/yolo_labelstudio_train/weights/best.pt"
+        image_dir = f"{sys.argv[1]}/images/testing"
+        classify_images(model_path, image_dir)
