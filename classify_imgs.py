98757182d83a0f6dd4d4450e21021b962b5e5233
toneja <toneyja@oregonstate.edu>
2024-12-09 16:02:38 -0800

    classify: use model.predict and save the results

--- classify_imgs.py
+++ classify_imgs.py
@@ -14,13 +14,13 @@ def classify_images(model_path, image_dir):
         if not image_name.lower().endswith((".jpg", ".jpeg")):
             continue
         # Evaluate each image
-        results = model(image_path)
-        print(results)
+        results = model.predict(image_path, save=True, imgsz=1024, conf=0.5)
         print(f"Processed {image_name}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
-        model_path = f"{sys.argv[1]}/runs/train/yolo_labelstudio_train/weights/best.pt"
-        image_dir = f"{sys.argv[1]}/images/testing"
+        project = sys.argv[1]
+        model_path = f"{project}/runs/train/yolo_labelstudio_train/weights/best.pt"
+        image_dir = f"{project}/images/testing"
         classify_images(model_path, image_dir)
